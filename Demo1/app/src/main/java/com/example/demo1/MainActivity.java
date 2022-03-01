cd2409d91151cb107d0894db0df9275d2d1b32f2
Polumm <songchujiashenzhen@163.com>
2022-03-01 21:05:00 +0800

    2021

--- Demo1/app/src/main/java/com/example/demo1/MainActivity.java
+++ Demo1/app/src/main/java/com/example/demo1/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.demo1;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+import androidx.appcompat.app.AppCompatActivity;
+//注意import开发所需的库
 
 public class MainActivity extends AppCompatActivity {
-
+    //首先，需要MapView指针随时调用我们的地图
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //初始化上下文
+        SDKInitializer.initialize(getApplicationContext());
+        //初始化坐标系
+        SDKInitializer.setCoordType(CoordType.BD09LL);
+        //设置布局
         setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.bmapView);
     }
 }
